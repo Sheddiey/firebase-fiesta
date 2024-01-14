b7d71106763a6a52aa2cf380bad58d4a9e56c443
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2024-01-14 04:35:17 +0300

    Finished database data write implementation.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import { useEffect, useState } from "react";
 import Auth from "./Components/Auth";
-import { collection, getDocs } from "firebase/firestore";
+import { addDoc, collection, getDocs } from "firebase/firestore";
 import { db } from "./config/firebase";
 
 const App = () => {
   const [movieList, setMovieList] = useState([]);
+
+  // New movie states
+  const [newMovieTitle, setNewMovieTitle] = useState("");
+  const [newReleaseDate, setNewReleaseDate] = useState("");
+  const [isNewMovieOscar, setIsNewMovieOscar] = useState(false);
+
   const movieCollectionRef = collection(db, "movies");
 
   const getMovieList = async () => {
@@ -12,28 +18,60 @@ const App = () => {
       const data = await getDocs(movieCollectionRef);
       const filteredData = data.docs.map((doc) => ({
         ...doc.data(),
-        id: doc.id
-      }))
+        id: doc.id,
+      }));
       setMovieList(filteredData);
     } catch (err) {
       console.error(err);
     }
-  }
+  };
 
-  useEffect(() =>{
-    getMovieList()
-  }, [])
+  useEffect(() => {
+    getMovieList();
+  }, []);
+
+  const onSubmitMovie = async () => {
+    try {
+      await addDoc(movieCollectionRef, {
+        title: newMovieTitle,
+        releaseDate: newReleaseDate,
+        receivedAnOscar: isNewMovieOscar,
+      })
+      getMovieList();
+    } catch(err) {
+      console.error(err);
+    }
+  };
 
   return (
     <div>
       <Auth />
+
       <div>
-      {movieList.map((movie) => (
-        <div>
-          <h1 style={{color: movie.receivedAnOscar ? 'green' : 'red'}}>{movie.title}</h1>
-          <p>Date: {movie.releaseDate}</p>
-        </div>
-      ))}
+        <input
+          type="text"
+          placeholder="Movie title..."
+          onChange={(e) => setNewMovieTitle(e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Release date..."
+          onChange={(e) => setNewReleaseDate(Number(e.target.value))}
+        />
+        <input type="checkbox" checked={isNewMovieOscar} onChange={(e) => setIsNewMovieOscar(e.target.checked)}/>
+        <label htmlFor="">Received an Oscar</label>
+        <button onClick={onSubmitMovie}>Submit Movie</button>
+      </div>
+
+      <div>
+        {movieList.map((movie) => (
+          <div key={movie.id}>
+            <h1 style={{ color: movie.receivedAnOscar ? "green" : "red" }}>
+              {movie.title}
+            </h1>
+            <p>Date: {movie.releaseDate}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
