4f062ee3bd4676e86d40ab771591fc45aff380f5
Shadrack <88189465+Sheddiey@users.noreply.github.com>
2024-01-14 03:26:39 +0300

    Implemented database data retrieval functionality.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
+import { useEffect, useState } from "react";
 import Auth from "./Components/Auth";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "./config/firebase";
 
 const App = () => {
+  const [movieList, setMovieList] = useState([]);
+  const movieCollectionRef = collection(db, "movies");
+
+  const getMovieList = async () => {
+    try {
+      const data = await getDocs(movieCollectionRef);
+      const filteredData = data.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id
+      }))
+      setMovieList(filteredData);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  useEffect(() =>{
+    getMovieList()
+  }, [])
+
   return (
     <div>
       <Auth />
+      <div>
+      {movieList.map((movie) => (
+        <div>
+          <h1 style={{color: movie.receivedAnOscar ? 'green' : 'red'}}>{movie.title}</h1>
+          <p>Date: {movie.releaseDate}</p>
+        </div>
+      ))}
+      </div>
     </div>
   );
 };
